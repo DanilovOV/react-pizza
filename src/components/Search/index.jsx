a6c13f552279236ac02c24d74dbf0c14131509ad
Oleg Danilov <oleg-danilov8@ya.ru>
2022-05-28 22:25:46 +0300

    add-context

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+
+import { SearchContext } from '../../App';
+
 import styles from './Search.module.scss';
 import clearIcon from '../../assets/img/clearIcon.svg';
-const Search = ({ searchValue, setSearchValue }) => {
+
+const Search = () => {
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
   return (
     <div className={styles.root}>
       <svg
