7e48903bd4f1f02fbc0b2aac62a19436443cacb8
Oleg Danilov <oleg-danilov8@ya.ru>
2022-06-02 09:33:08 +0300

    add-svg

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,6 +4,7 @@ import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 import clearIcon from '../../assets/img/clearIcon.svg';
+import findIcon from '../../assets/img/find.svg';
 
 const Search = () => {
   const [inputValue, setInputValue] = React.useState(''); // для отображения в инпуте
@@ -30,39 +31,7 @@ const Search = () => {
 
   return (
     <div className={styles.root}>
-      <svg
-        className={styles.icon}
-        enableBackground="new 0 0 32 32"
-        id="EditableLine"
-        version="1.1"
-        viewBox="0 0 32 32"
-        xmlns="http://www.w3.org/2000/svg">
-        <circle
-          cx="14"
-          cy="14"
-          fill="none"
-          id="XMLID_42_"
-          r="9"
-          stroke="#000000"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeMiterlimit="10"
-          strokeWidth="2"
-        />
-        <line
-          fill="none"
-          id="XMLID_44_"
-          stroke="#000000"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeMiterlimit="10"
-          strokeWidth="2"
-          x1="27"
-          x2="20.366"
-          y1="27"
-          y2="20.366"
-        />
-      </svg>
+      <img src={findIcon} className={styles.icon} alt="" />
       <input
         ref={inputRef}
         className={styles.input}
